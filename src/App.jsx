3a3123cb5029d4fce63cd33d9558a7a3baf9a0a5
Shubham Panchal <panchalshubham826@gmail.com>
2022-09-01 11:24:50 +0530

    Now Change state var of child comp from parent comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import UseStateHook from "./HookBuster/useStateHook";
+import ChildComp from "./HookBuster/BustHooks";
+import React from "react";
 import { useRef } from "react";
 function App() {
-  const myBtn = useRef(null);
   // useState under the hood
   // console.log(useState(100)); //  [100, ƒ]
   // It returns an array with a variable and a function which comes vary helpful in changing the variable
-  const clickIt = (myBtn) => myBtn();
+
+  //Note: Refs cannot be passed as simply props to functional components.
+  const ref = useRef();
+  const clickIt = () => ref.current.click();
+  const greetFunc = (greet) => console.log(greet);
   return (
     <div>
-      <button >ClickMe</button>
-      <UseStateHook click={clickIt} />
+      <div>Change child state vars from parent</div>
+      <button onClick={() => clickIt()}>Change'em</button>
+      <ChildComp ref={ref} greetFunc={greetFunc}>
+        Click me!
+      </ChildComp>
     </div>
   );
 }
